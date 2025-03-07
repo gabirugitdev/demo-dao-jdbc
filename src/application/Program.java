83837c08ab8c92d13bfd777e77b16fcab538069d
gabiru <96029469+gabirugitdev@users.noreply.github.com>
2025-03-06 22:14:51 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,9 +13,12 @@ public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("-- findById --");
         Seller seller = sellerDao.findById(1);
         System.out.println(seller);
-        
+
+        System.out.println("-- findByDepartment --");
         Department department = new Department(2, null);
         List<Seller> sellers = sellerDao.findByDepartment(department);
 
@@ -23,5 +26,13 @@ public class Program {
             System.out.println(s);
         }
 
+        System.out.println("-- findAll --");
+        List<Seller> list = sellerDao.findAll();
+
+        for(Seller s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("");
     }
 }
